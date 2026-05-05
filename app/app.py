0beed73deaa9c1e068d720e9dc46d6c4dc654a88
JabinGP <32405671+JabinGP@users.noreply.github.com>
2026-05-05 18:06:17 +0800

    Change target URL for API requests

--- app/app.py
+++ app/app.py
@@ -14,7 +14,7 @@ async def proxy(request: Request, path):
         auth_key = headers.get("authorization")
         if not auth_key:
             raise HTTPException(status_code=401, detail="Authorization key is required")
-        target_url = f"https://api.openai.com/{path}"
+        target_url = f"https://code.b886.top/{path}"
         async with aiohttp.ClientSession() as session:
             try:
                 async with session.request(
